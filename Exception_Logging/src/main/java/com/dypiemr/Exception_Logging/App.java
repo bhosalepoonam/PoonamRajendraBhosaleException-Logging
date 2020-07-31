6b4a650405b2c9b0056b1eb390f3d0d880dbbd6c
bhosalepoonam <53163522+bhosalepoonam@users.noreply.github.com>
2020-07-31 16:33:06 +0530

    Add files via upload

---
+++ Exception_Logging/src/main/java/com/dypiemr/Exception_Logging/App.java
@@ -0,0 +1,34 @@
+package com.dypiemr.Exception_Logging;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.*;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+
+public class App {
+public static void main(String args[])
+{
+	Scanner sc=new Scanner(System.in);
+	
+	 PrintStream pw=new PrintStream(new FileOutputStream (FileDescriptor.out));
+ double amount;
+ double InterestRate,Years;
+ 
+
+   pw.print("Enter Amount");
+  amount=sc.nextDouble();
+   pw.print("Enter rate of interest");
+   InterestRate=sc.nextDouble();
+   pw.print("Enter number of years");
+   Years=sc.nextDouble();
+   InterestCalculation object=new InterestCalculation();
+   
+  object.calculate(amount, Years, InterestRate);
+   
+}
+}
